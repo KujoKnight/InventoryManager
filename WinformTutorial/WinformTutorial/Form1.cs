0e09cce811c546101d0b4a2b28312ab44548b4f8
John <kujoknight@gmail.com>
2019-11-06 21:33:00 -0500

    Part 6 of Matching Game tutorial

--- WinformTutorial/WinformTutorial/Form1.cs
+++ WinformTutorial/WinformTutorial/Form1.cs
@@ -45,6 +45,9 @@ namespace WinformTutorial
 
         private void label_Click(object sender, EventArgs e)
         {
+            if (timer1.Enabled == true)
+                return;
+
             Label clickedLabel = sender as Label;
 
             if (clickedLabel != null)
@@ -56,10 +59,25 @@ namespace WinformTutorial
                 {
                     firstClicked = clickedLabel;
                     firstClicked.ForeColor = Color.Black;
-
                     return;
                 }
+
+                secondClicked = clickedLabel;
+                secondClicked.ForeColor = Color.Black;
+
+                timer1.Start();
             }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Stop();
+
+            firstClicked.ForeColor = firstClicked.BackColor;
+            secondClicked.ForeColor = secondClicked.BackColor;
+
+            firstClicked = null;
+            secondClicked = null;
         }
     }
 }
