c042a98d76aae28a4a179ec4599592f9ae8166aa
John <kujoknight@gmail.com>
2019-11-06 21:30:21 -0500

    Part 5 of Matching Game tutorial

--- WinformTutorial/WinformTutorial/Form1.cs
+++ WinformTutorial/WinformTutorial/Form1.cs
@@ -12,6 +12,9 @@ namespace WinformTutorial
 {
     public partial class Form1 : Form
     {
+        Label firstClicked = null;
+        Label secondClicked = null;
+
         Random random = new Random();
         List<string> icons = new List<string>() 
         {
@@ -49,7 +52,13 @@ namespace WinformTutorial
                 if (clickedLabel.ForeColor == Color.Black)
                     return;
 
-                clickedLabel.ForeColor = Color.Black;
+                if (firstClicked == null)
+                {
+                    firstClicked = clickedLabel;
+                    firstClicked.ForeColor = Color.Black;
+
+                    return;
+                }
             }
         }
     }
